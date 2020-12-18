60e1a5a8652f05ea30b90d3939728cf6a92e56f6
Danilo Duarte <danilo.duarte8@hotmail.com>
2020-12-18 09:02:54 -0300

    chore(package): add expo-font and @expo-google-fonts/roboto

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { AppLoading } from 'expo';
+import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
 
 import Routes from './src/routes';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <NavigationContainer>
       <StatusBar />
